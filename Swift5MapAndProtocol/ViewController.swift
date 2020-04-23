47953a52e09672f89fadc33d7dda8a77fae70f9b
Nonoka Yamamoto <garden.n1234567890@gmail.com>
2020-04-24 01:08:56 +0900

    Initial Commit

---
+++ Swift5MapAndProtocol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5MapAndProtocol
+//
+//  Created by 山本ののか on 2020/04/24.
+//  Copyright © 2020 Nonoka Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
